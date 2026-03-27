b19a8147c9183b1a01fd56cd3f23a3311a335d95
sagarpatil <sagarpatil06180@gmail.com>
2026-03-27 10:30:20 +0530

    Icons.jsx deleted

--- src/assets/Icons.jsx
+++
@@ -1,5 +0,0 @@
-// You import from sub-folders (lu = Lucide, fi = Feather, fa = FontAwesome)
-import { LuUser, LuCalendar, LuBarChart } from "react-icons/lu";
-
-export const UserIcon = () => <LuUser />;
-export const CalendarIcon = () => <LuCalendar />;
